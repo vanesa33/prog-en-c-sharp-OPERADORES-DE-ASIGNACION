ffcea8d3bdb6c846184c8304d385971e44828ed0
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-09 14:21:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace operadores_de_asignacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte edad = 20;
+
+            edad += 10;
+
+            Console.WriteLine("tu edad es= " + edad );
+
+            Console.ReadKey();
+
+        }
+    }
+}
